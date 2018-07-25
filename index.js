f74cbab6b6f53d9a8aa53754b24be939c2d6ce8a
Prem Bikram Limbu <prem.bikram.limbu@gmail.com>
2018-07-26 02:58:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 // Get index table details from Json
-(function(){
-  load('product.json');
-})()
-function load(url) {
+let load = (url) => {
   let xhr = new XMLHttpRequest();
   xhr.open('GET', url);
   xhr.timeout = 1000;
@@ -11,9 +8,9 @@ function load(url) {
   xhr.onload = function() {
     //console.log(this.responseText);
     if(this.status==200){
-      var result_json = JSON.parse(this.responseText);
-      var html_template = '';
-      for (var key in result_json) {
+      let result_json = JSON.parse(this.responseText);
+      let html_template = '';
+      for (let key in result_json) {
         if (result_json.hasOwnProperty(key)) {
           html_template += '<tr><td class="sn">'+result_json[key].prod_sn+'</td>'+
           '<td class="id">'+result_json[key].prod_id+'</td>'+
@@ -22,7 +19,6 @@ function load(url) {
           '<td class="snippet">'+result_json[key].prod_snippet+'</td></tr>';
         }
       }
-      html_template += '<tr><ul class="pagination"></ul></tr>';
       document.getElementById("tablebody").innerHTML=html_template;
     }
   };
@@ -31,3 +27,4 @@ function load(url) {
 
   xhr.ontimeout = () => alert('Timeout!');
 }
+load('product.json');
