33beb13ce3186191d07d46d9d01f58f3d289e953
Prem Bikram Limbu <prem.bikram.limbu@gmail.com>
2018-07-26 02:44:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
+// Get index table details from Json
 (function(){
   load('product.json');
 })()
-
 function load(url) {
   let xhr = new XMLHttpRequest();
   xhr.open('GET', url);
@@ -9,22 +9,25 @@ function load(url) {
   xhr.send();
 
   xhr.onload = function() {
-    alert(`Loaded: ${this.status} ${this.responseText}`);
-    var context = {sn: "My New Post",
-                    id: "First post!",
-                    imageUrl: "/img/image.png",
-                    name: "TEST NAME",
-                    snippet: "DESCRIPTION ..."};
-    var html    = template(context);
+    //console.log(this.responseText);
+    if(this.status==200){
+      var result_json = JSON.parse(this.responseText);
+      var html_template = '';
+      for (var key in result_json) {
+        if (result_json.hasOwnProperty(key)) {
+          html_template += '<tr><td class="sn">'+result_json[key].prod_sn+'</td>'+
+          '<td class="id">'+result_json[key].prod_id+'</td>'+
+          '<td class="imageUrl">'+result_json[key].prod_imageUrl+'</td>'+
+          '<td class="name">'+result_json[key].prod_name+'</td>'+
+          '<td class="snippet">'+result_json[key].prod_snippet+'</td></tr>';
+        }
+      }
+      html_template += '<tr><ul class="pagination"></ul></tr>';
+      document.getElementById("tablebody").innerHTML=html_template;
+    }
   };
 
   xhr.onerror = () => alert('Error');
 
   xhr.ontimeout = () => alert('Timeout!');
 }
-
-var options = {
-  valueNames: [ 'sn', 'id', 'imageUrl', 'name', 'snippet' ]
-};
-
-var userList = new List('products', options);
